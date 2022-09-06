79ebff5a3603be64c04babe59ea7835b378d82d1
Cris <crisguycabs@gmail.com>
2022-09-06 17:53:23 -0500

    Acerca de modificado

--- acercadeChild.py
+++ acercadeChild.py
@@ -2,11 +2,13 @@
 
 # Form implementation generated from reading ui file 'acercadeChild.ui'
 #
-# Created by: PyQt5 UI code generator 5.9.2
+# Created by: PyQt5 UI code generator 5.12.3
 #
 # WARNING! All changes made in this file will be lost!
 
+
 from PyQt5 import QtCore, QtGui, QtWidgets
+
 
 class Ui_Acercade(object):
     def setupUi(self, Acercade):
@@ -22,21 +24,11 @@ class Ui_Acercade(object):
         self.label.setGeometry(QtCore.QRect(10, 10, 311, 171))
         self.label.setObjectName("label")
         self.label_2 = QtWidgets.QLabel(Acercade)
-        self.label_2.setGeometry(QtCore.QRect(250, 10, 191, 71))
+        self.label_2.setGeometry(QtCore.QRect(250, 70, 191, 71))
         self.label_2.setText("")
         self.label_2.setPixmap(QtGui.QPixmap("ecopetrol.png"))
-        self.label_2.setScaledContents(True) 
+        self.label_2.setScaledContents(True)
         self.label_2.setObjectName("label_2")
-        # self.label_3 = QtWidgets.QLabel(Acercade)
-        # self.label_3.setGeometry(QtCore.QRect(240, 90, 201, 101))
-        # self.label_3.setText("")
-        # self.label_3.setPixmap(QtGui.QPixmap("tip.jpg"))
-        # self.label_3.setScaledContents(False)
-        # self.label_3.setObjectName("label_3")
-        self.buttonBox.raise_()
-        self.label.raise_()
-        # self.label_3.raise_()
-        self.label_2.raise_()
 
         self.retranslateUi(Acercade)
         self.buttonBox.accepted.connect(Acercade.accept)
@@ -46,7 +38,7 @@ class Ui_Acercade(object):
     def retranslateUi(self, Acercade):
         _translate = QtCore.QCoreApplication.translate
         Acercade.setWindowTitle(_translate("Acercade", "Acerca de..."))
-        self.label.setText(_translate("Acercade", "<html><head/><body><p><span style=\" font-style:italic;\">Diseñado por </span></p><p>Ph.D. (c) Crisostomo A. Barajas-Solano, TIP Colombia</p><p>M.Sc. Jorge Diaz, TIP Colombia</p><p>M.Sc. Luz Angela Bohorquez, TIP Colombia</p><p><br/><span style=\" font-style:italic;\">Bajo la dirección de</span></p><p>Ph.D. (c) Ariel Uribe, Ecopetrol S.A.</p><p>Ing. Luz Marina Rozo, TIP Colombia</p></body></html>"))
+        self.label.setText(_translate("Acercade", "<html><head/><body><p><span style=\" font-family:\'Humanst521 BT\',\'sans-serif\'; font-size:11pt; font-weight:600;\">Centro de Innovación y Tecnología Instituto Colombiano del Petróleo ICP</span></p><p><span style=\" font-weight:600;\">Grupo de Análisis de Sistemas Energéticos</span></p><p><span style=\" font-weight:600;\">Contacto</span>: ariel.uribe@ecopetrol.com.co </p><p><span style=\" font-weight:600;\">Desarrollado por TIP LTDA: </span></p><p>Ph.D.(c) Cristomo A Barajas Solano </p><p>Msc.(c) Jorge E Diaz </p><p>Ing. Luz A Bohorquez Ballesteros </p></body></html>"))
 
 
 if __name__ == "__main__":
@@ -57,4 +49,3 @@ if __name__ == "__main__":
     ui.setupUi(Acercade)
     Acercade.show()
     sys.exit(app.exec_())
-
